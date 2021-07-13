ee861187161e0141b7d770ca762b5acd567178af
Alexandre Bohyn <u0133728@kuleuven.be>
2021-07-13 22:37:43 +0200

    Initial commit

---
+++ R/editions.R
@@ -0,0 +1,16 @@
+#' Information the Giro d'Italia
+#'
+#' A dataset containing informations about all the previous editions (from 1909 to 2021)
+#' of the Giro d'Italia, one of the 3 grand tours in cycling.
+#'
+#' @format A dataframe with 113 rows and 6 variables:
+#' \describe{
+#' \item{year}{year of the edition}
+#' \item{gc_list}{general classification results, including rider name, team, time and bib number}
+#' \item{date}{date of the start of the race, in %d %b %Y format}
+#' \item{arrival}{location of the end of the race}
+#' \item{departure}{location of the start of the race}
+#' }
+#'
+#' @source \url{https://www.procyclingstats.com/race/giro-d-italia}
+"editions"

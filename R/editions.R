d1b9a5d108945e08665bea091bc41adeee0ca139
Alexandre Bohyn <u0133728@kuleuven.be>
2021-07-14 09:47:16 +0200

    modified data description

--- R/editions.R
+++ R/editions.R
@@ -1,16 +1,17 @@
-#' Information the Giro d'Italia
+#' Il Giro d'Italia
 #'
-#' A dataset containing informations about all the previous editions (from 1909 to 2021)
-#' of the Giro d'Italia, one of the 3 grand tours in cycling.
+#' A dataset containing informations about winners and stage results for all the
+#' previous editions (from 1909 to 2021) of the Giro d'Italia, one of the 3
+#' grand tours in cycling.
 #'
-#' @format A dataframe with 113 rows and 6 variables:
+#' @format A data frame with 113 rows and 6 variables
 #' \describe{
-#' \item{year}{year of the edition}
-#' \item{gc_list}{general classification results, including rider name, team, time and bib number}
-#' \item{date}{date of the start of the race, in %d %b %Y format}
-#' \item{arrival}{location of the end of the race}
-#' \item{departure}{location of the start of the race}
+#'   \item{year}{year of the edition}
+#'   \item{speed}{average speed of the winner (in km/h)}
+#'   \item{gc_list}{general classification including rider, team, BIB number and time}
+#'   \item{date}{date of the start (Object of "Date" class)}
+#'   \item{arrival}{location of the end of the race}
+#'   \item{departure}{location of the start of the race}
 #' }
-#'
 #' @source \url{https://www.procyclingstats.com/race/giro-d-italia}
 "editions"
